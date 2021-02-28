88068027f5ab7f10b0b08bf5a845f0c9111449fd
Viktoriia Fenenko <v.l.fenenko@gmail.com>
2021-02-28 10:33:12 +0200

    карточки галереии

--- js/gallery_cards.js
+++ js/gallery_cards.js
@@ -1,28 +1,33 @@
 'user strict'
-import defaultEl from "./gallery-items";
+import galleryFotos from "./gallery_items.js";
 
 const galleryContainer = document.querySelector('.js-gallery');
 const galleryCards = createGalleryCards(galleryFotos);
-galleryContainer.insertAdjacentHTML('beforeend', galleryCards)
+ galleryContainer.insertAdjacentHTML('beforeend', createGalleryCards(galleryFotos))
 
-function createGalleryCards (galleryFotos) {
+ console.log(galleryContainer);
+
+function createGalleryCards(galleryFotos) {
   return galleryFotos
-    .map(({ preview, original, description }) => {
-      return
-      `  <li class="gallery__item">
+    .map(({ preview, original, description}) => {
+      return `
+   <li class="gallery__item">
   <a
     class="gallery__link"
-    href="${original}"
-  >
+    href=${original} >
     <img
       class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+      src=${preview}
+      data-source=${original}
+      alt=${description}R
     />
   </a>
-</li>`
+</li>
+    `;
     })
     .join('');
-
 }
+
+
+
+
--- js/gallery_items.js
+++ js/gallery_items.js
@@ -1,4 +1,4 @@
-export default  [
+export default [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,8 +63,3 @@ export default  [
     description: 'Lighthouse Coast Sea',
   },
 ];
-
-
-
-
-
