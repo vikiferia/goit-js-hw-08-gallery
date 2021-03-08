5a35243efc4e78957f8e7a8d4ed95afd3cb1018f
Viktoriia Fenenko <v.l.fenenko@gmail.com>
2021-03-08 15:40:41 +0200

    создание галереи

--- js/gallery_cards.js
+++ js/gallery_cards.js
@@ -1,14 +1,17 @@
 'user strict'
-import galleryFotos from "./gallery_items.js";
+import images  from "./gallery_items.js";
 
-const galleryContainer = document.querySelector('.js-gallery');
-const galleryCards = createGalleryCards(galleryFotos);
- galleryContainer.insertAdjacentHTML('beforeend', createGalleryCards(galleryFotos))
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  image: document.createElement('img'),
+  lightbox: document.querySelector('.lightbox'),
+  btn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector('.lightbox__content'),
+  lightbox__image: document.querySelector('.lightbox__image'),
+};
 
- console.log(galleryContainer);
-
-function createGalleryCards(galleryFotos) {
-  return galleryFotos
+function createGalleryCards(images) {
+  return images
     .map(({ preview, original, description}) => {
       return `
    <li class="gallery__item">
@@ -19,15 +22,46 @@ function createGalleryCards(galleryFotos) {
       class="gallery__image"
       src=${preview}
       data-source=${original}
-      alt=${description}R
+      alt=${description}
     />
   </a>
 </li>
     `;
     })
     .join('');
+  
+};
+
+
+refs.gallery.insertAdjacentHTML('afterbegin', createGalleryCards(images));
+refs.gallery.addEventListener('click', onGallery);
+refs.btn.addEventListener('click', galleryClose);
+refs.modal.addEventListener('click', closeLightbox);
+
+function onGallery(event) {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+   refs.lightbox.classList.add('is-open');
+    refs.lightbox__image.src = event.target.getAttribute('data-source');
+    refs.lightbox__image.alt = event.target.alt;
+  
 }
 
+function galleryClose(event) {
+  event.preventDefault(); 
+  refs.lightbox.classList.remove('is-open');
+  refs.lightbox__image.src = '';
+  refs.lightbox__image.alt = '';
+  
+}
 
-
+function closeLightbox(event) {
+  if (event.target === event.currentTarget) {
+    galleryClose();
+  }
+}
 
